bccff5461de3181cbee4a05ceb15309421a770a0
Anton Vakhrushev <anwinged@ya.ru>
2019-10-13 12:35:54 +0300

    Move point tests

--- src/game/map.cr
+++ src/game/map.cr
@@ -1,19 +1,4 @@
 module Game
-  struct Point
-    property x : Int32
-    property y : Int32
-
-    def initialize(@x : Int32, @y : Int32)
-    end
-
-    getter x
-    getter y
-
-    def distance(other) : Int32
-      return (other.x - @x).abs + (other.y - @y).abs
-    end
-  end
-
   class Map
     alias TileRow = Array(Tile)
     alias DataArray = Array(TileRow)
---
+++ src/game/point.cr
@@ -0,0 +1,14 @@
+struct Game::Point
+  property x : Int32
+  property y : Int32
+
+  def initialize(@x : Int32, @y : Int32)
+  end
+
+  getter x
+  getter y
+
+  def distance(other) : Int32
+    return (other.x - @x).abs + (other.y - @y).abs
+  end
+end
